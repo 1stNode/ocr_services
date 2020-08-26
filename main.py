86d83a3dff270d88371975efc8cace36c7901768
1stNode <70192264+1stNode@users.noreply.github.com>
2020-08-26 16:37:56 +0800

    Add files via upload
    first init repo.

---
+++ main.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+import os
+import sys
+import cv2
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+if os.getcwd() not in sys.path:
+    sys.path.append(os.getcwd())
+
+from service.income_service import IncomeService
+from service.cheque_service import ChequeService
+from service.rec_service import RecService
+
+
+if __name__ == "__main__":
+    income_image = cv2.imread('test_datas/002.jpg')
+    cheque_image = cv2.imread('test_datas/001.jpg')
+    image = cv2.imread('test_datas/0_0.jpg')
+
+    income_service = IncomeService()
+    i_result = income_service.image_pred([income_image])
+    print(i_result)
+
+    cheque_service = ChequeService()
+    c_result = cheque_service.image_pred([cheque_image])
+    print(c_result)
+
+    rec_service = RecService()
+    r_result = rec_service.image_pred([image])
+    print(r_result)
+
---
+++ text_processing/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# -*- encoding: utf-8 -*-
+
+"""
+@Author  :   Wingsdh
+@File    :   __init__.py.py
+@Time    :   2020-2-27 9:25
+@Desc    :
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
